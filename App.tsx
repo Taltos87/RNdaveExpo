6c623d1d66b8237cab1396c165e099de4cd06350
Taltos87 <93266473+Taltos87@users.noreply.github.com>
2023-05-04 21:19:40 +0100

    Update app.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+abstract class Shape {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Circle extends Shape {
+  override render() {
+   
+ }
+  }
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Write your message here</Text
+
       <StatusBar style="auto" />
     </View>
   );
